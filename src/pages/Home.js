edc9e8acb98125d6f7cec242d5d0f5824c306123
Smartpro94 <matthieu.barilone@gmail.com>
2024-11-12 08:32:42 +0100

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Accueil</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum suscipit
+        quisquam laboriosam dignissimos nulla accusamus amet delectus iure
+        provident nesciunt. Reiciendis laborum veniam soluta natus itaque, enim,
+        placeat a fugiat animi, maiores officiis ducimus repudiandae. Aperiam
+        consectetur natus quis repellendus, reprehenderit ipsa, molestias,
+        adipisci iusto dignissimos magnam pariatur qui. Eveniet nulla nisi odit
+        facere? Repellendus quisquam aperiam, consequuntur dolorem similique
+        libero. Deleniti, necessitatibus dicta. Ratione repudiandae animi ab
+        quisquam quia rem accusamus nam maiores provident, explicabo aut dicta,
+        exercitationem culpa esse libero, nemo rerum nesciunt delectus. Et illum
+        qui eos dolor, nam totam, eum cumque aspernatur quam ducimus amet
+        dolores placeat. Quae placeat aliquam culpa ut unde officia consequatur
+        earum animi sed id iste voluptate sapiente minus dolores soluta eveniet
+        aspernatur est, ab, pariatur quos labore totam explicabo. Ducimus
+        expedita nesciunt omnis placeat nostrum odio necessitatibus at
+        accusantium, vel quae eligendi reprehenderit id saepe facere corporis
+        esse animi atque sequi alias eum delectus dolore. Explicabo ducimus et
+        nobis ex in dicta iure aut reiciendis molestias laborum neque rem,
+        debitis tempore mollitia, tenetur tempora ea, aperiam veritatis ad
+        maxime obcaecati voluptatum dignissimos beatae. Temporibus est excepturi
+        quo quia reprehenderit nostrum non dolor dolores dolorum iusto, laborum
+        deserunt quaerat deleniti recusandae ipsum.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
