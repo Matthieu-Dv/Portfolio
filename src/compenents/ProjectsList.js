da1e7f70f392d13d8a4a43d1f9aa97d84d447014
Smartpro94 <matthieu.barilone@gmail.com>
2024-12-04 09:38:55 +0100

    mise en place du filtrage sur la page projets

--- src/compenents/ProjectsList.js
+++ src/compenents/ProjectsList.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 // Data
 import projects from '../projects.json';
-//FontAwesome
+// FontAwesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
 
 const ProjectsList = () => {
+  const [filter, setFilter] = useState(null);
+  // État pour le filtre
+
+  // Fonction pour filtrer les projets
+  const filteredProjects = filter
+    ? projects.filter((project) => project.type.includes(filter))
+    : projects;
+
   return (
     <>
       <section className="filter">
         <ul>
-          <li>Front-End</li>
-          <li>SEO</li>
+          <li
+            onClick={() => setFilter(null)}
+            className={filter === null ? 'click' : ''}
+          >
+            Tous
+          </li>{' '}
+          {/* Affiche tous les projets */}
+          <li
+            onClick={() => setFilter('Front-End')}
+            className={filter === 'Front-End' ? 'click' : ''}
+          >
+            Front-End
+          </li>
+          <li
+            onClick={() => setFilter('SEO')}
+            className={filter === 'SEO' ? 'click' : ''}
+          >
+            SEO
+          </li>
         </ul>
       </section>
       <section>
         <div className="projets">
-          {projects.map((project, index) => (
+          {filteredProjects.map((project, index) => (
             <div key={index} className="projet">
               <div className="image">
-                <img src={project.img} alt={project.title} />
+                <img src={project.img} alt={project.alt || 'Image du projet'} />
               </div>
               <div className="texte">
                 <div className="top">
                   {project.logo && (
-                    <img src={project.logo} alt="Logo du projet" />
+                    <img src={project.logo} alt={project.logoAlt} />
                   )}
                 </div>
                 <div className="bottom">
@@ -37,32 +63,51 @@ const ProjectsList = () => {
                     ))}
                   </div>
                   <div className="icon">
-                    <a
-                      href={project.github}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FontAwesomeIcon
-                        icon={faGithub}
-                        style={{ fontSize: '24px' }}
-                        title="Lien vers le repo github"
-                      />
-                    </a>
-                    <a
-                      href={project.site}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FontAwesomeIcon
-                        icon={faArrowUpRightFromSquare}
-                        style={{
-                          fontSize: '21px',
-                          color: 'black',
-                          paddingTop: '2px',
-                        }}
-                        title="Lien vers le site"
-                      />
-                    </a>
+                    {project.github && (
+                      <a
+                        href={project.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FontAwesomeIcon
+                          icon={faGithub}
+                          style={{ fontSize: '24px' }}
+                          title="Lien vers le repo github"
+                        />
+                      </a>
+                    )}
+                    {project.site && (
+                      <a
+                        href={project.site}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FontAwesomeIcon
+                          icon={faArrowUpRightFromSquare}
+                          style={{
+                            fontSize: '21px',
+                            paddingTop: '2px',
+                          }}
+                          title="Lien vers le site"
+                        />
+                      </a>
+                    )}
+                    {project.rapportOptimisation && (
+                      <a
+                        href={project.rapportOptimisation}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FontAwesomeIcon
+                          icon={faFilePdf}
+                          style={{
+                            fontSize: '21px',
+                            paddingTop: '2px',
+                          }}
+                          title="Lien vers le rapport d'optimisation"
+                        />
+                      </a>
+                    )}
                   </div>
                 </div>
               </div>
